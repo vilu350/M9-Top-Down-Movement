107c57cb82aa6c2c81dd51ff6ebda78049bf53a1
vilu350 <vilu@s.sfusd.edu>
2025-12-03 13:51:18 -0800

    Worked on M9L3

--- lessons/runner.gd
+++ lessons/runner.gd
@@ -1,5 +1,17 @@
 extends CharacterBody2D
 
+@onready var _skin: Sprite2D = %Skin
+
+const RUNNER = preload("uid://c4jjlttvtvjqc")
+const RUNNER_DOWN = preload("uid://c0i1ik45p7rhh")
+const RUNNER_DOWN_LEFT = preload("uid://bork38ywg3paf")
+const RUNNER_DOWN_RIGHT = preload("uid://cst3aklarj68")
+const RUNNER_LEFT = preload("uid://bk7kvspkijqac")
+const RUNNER_RIGHT = preload("uid://b4etxv4c5w1mq")
+const RUNNER_UP = preload("uid://dtrvq16cx035")
+const RUNNER_UP_LEFT = preload("uid://b5yil62vnj1o7")
+const RUNNER_UP_RIGHT = preload("uid://c7x3s5c2r5l86")
+
 var max_speed := 600.0
 
 func _physics_process(_delta: float) -> void:
