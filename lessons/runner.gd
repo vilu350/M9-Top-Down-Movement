dfa0a9244e7b6535750d15bd22fa33d1e2e050cf
vilu350 <vilu@s.sfusd.edu>
2025-12-17 22:45:08 -0800

    Finished M9L8

--- lessons/runner.gd
+++ lessons/runner.gd
@@ -1,4 +1,7 @@
+class_name Runner
 extends CharacterBody2D
+
+signal walked_to
 
 
 @export var max_speed := 600.0
@@ -32,3 +35,19 @@ func _physics_process(delta: float) -> void:
 	else:
 		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
 		_dust.emitting = false
+
+
+func walk_to(destination_global_position: Vector2) -> void:
+	var direction := global_position.direction_to(destination_global_position)
+	_runner_visual.angle = direction.orthogonal().angle()
+	_runner_visual.animation_name = RunnerVisual.Animations.WALK
+	_dust.emitting = true
+	var distance := global_position.distance_to(destination_global_position)
+	var duration := distance / (max_speed * 0.2)
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", destination_global_position, duration)
+	tween.finished.connect(func():
+		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		_dust.emitting = false
+		walked_to.emit()
+	)
