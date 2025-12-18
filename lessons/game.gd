21b47cae095d32612fff626a5f5aa433bfd461bf
vilu350 <vilu@s.sfusd.edu>
2025-12-17 23:07:27 -0800

    Finished M9L9

--- lessons/game.gd
+++ lessons/game.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var _finish_line: FinishLine = %FinishLine
+@onready var _runner: Runner = %Runner
+@onready var _count_down: CountDown = %CountDown
 
 
 func _ready() -> void:
@@ -14,13 +16,18 @@ func _ready() -> void:
 			_finish_line.global_position
 			+ Vector2(0, 64)
 		)
-		
 		runner.walk_to(destination_position)
 		runner.walked_to.connect(
 			_finish_line.pop_confettis
 		)
 	)
-	
 	_finish_line.confettis_finished.connect(
 		get_tree().reload_current_scene.call_deferred
+	)
+	
+	_count_down.start_counting()
+	_runner.set_physics_process(false)
+	_count_down.counting_finished.connect(
+		func() -> void:
+			_runner.set_physics_process(true)
 	)
